786e48de08e8bfe65b5561447e4a5f3a528c48c1
Himanshu Singh <singhh208@gmail.com>
2025-06-19 05:11:38 +0000

    user limit part complete

--- hw-intro/limits.c
+++ hw-intro/limits.c
@@ -3,8 +3,32 @@
 
 int main() {
     struct rlimit lim;
-    printf("stack size: %ld\n", 0L);
-    printf("process limit: %ld\n", 0L);
-    printf("max file descriptors: %ld\n", 0L);
+    int result = getrlimit(RLIMIT_STACK,&lim);
+    if(result == 0){
+      if(lim.rlim_cur == RLIM_INFINITY){
+        printf("stack size(soft): INFINITY\n");
+      }
+      else{
+        printf("stack size(soft): %ld\n", lim.rlim_cur);
+      }
+    }
+    result = getrlimit(RLIMIT_NPROC,&lim);
+    if(result == 0){
+      if(lim.rlim_cur == RLIM_INFINITY){
+        printf("process limit(sof): INFINITY\n");
+      }
+      else{
+        printf("process limit(sof): %ld\n", lim.rlim_cur);
+      }
+    }
+    result = getrlimit(RLIMIT_NOFILE,&lim);
+    if(result == 0){
+      if(lim.rlim_cur == RLIM_INFINITY){
+        printf("max file descriptors(soft): INFINITY\n");
+      }
+      else{
+      printf("max file descriptors(soft): %ld\n", lim.rlim_cur);
+      }
+    }
     return 0;
 }
